3003c356dbb0f443e0802b594a1c6822f5466f16
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-10-26 19:21:49 +0600

    Fix name case for constants

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,18 +1,17 @@
 import tweepy
-import os
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-load_dotenv()
+CONFIG = dotenv_values('.env')
 
-consumer_key = os.environ.get("TWITTER_KEY")
-consumer_secret = os.environ.get("TWITTER_SECRET")
-access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
-access_token_secret = os.environ.get("TWITTER_ACCESS_SECRET")
+CONSUMER_KEY = CONFIG["TWITTER_KEY"]
+CONSUMER_SECRET = CONFIG["TWITTER_SECRET"]
+ACCESS_TOKEN = CONFIG["TWITTER_ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = CONFIG["TWITTER_ACCESS_SECRET"]
 
 
 def post_tweet(tweet_text, image):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     api.update_status_with_media(
         status=tweet_text, file=image, filename="contributor.png"
