7c32ff1cb9bc628e0486094e78562e1eca9ae25e
Omar Moustafa <omar.moustafa@toolsandapps4us.site>
2022-10-28 14:35:09 +0400

    clean the code and fix things

--- models/contributor.py
+++ models/contributor.py
@@ -3,10 +3,10 @@ from io import BytesIO
 from typing import Dict
 
 import aiohttp
-import numpy as np
-from twitter import *
 from discord_webhook import DiscordWebhook
 from PIL import Image, ImageDraw, ImageFont
+from twitter import OAuth, Twitter
+
 from src import global_
 
 from .organization import Organization
@@ -54,22 +54,7 @@ class Contributor:
                 org_avatar_bytes = BytesIO(await response2.read())
                 org_avatar = Image.open(org_avatar_bytes).resize((80, 80))
 
-                height, width = org_avatar.size
-                lum_img = Image.new("L", [height, width], 0)
-
-                org_draw = ImageDraw.Draw(lum_img)
-                org_draw.pieslice([(0, 0), (height, width)], 0, 360, fill=255)
-                org_avatar_arr = np.array(org_avatar)
-                lum_img_arr = np.array(lum_img)
-
-                final_org_avatar = Image.fromarray(
-                    np.dstack((org_avatar_arr, lum_img_arr))
-                )
-
-                try:
-                    image.paste(final_org_avatar, (60, 28), mask=final_org_avatar)
-                except ValueError:
-                    image.paste(final_org_avatar, (60, 28))
+                image.paste(org_avatar, (60, 28), org_avatar.convert("RGBA"))
 
         draw = ImageDraw.Draw(image)
 
@@ -122,7 +107,7 @@ class Contributor:
         )
 
         overlay = Image.open("assets/overlay.png")
-        image.paste(overlay, mask=overlay)
+        image = Image.alpha_composite(image, overlay)
 
         buffer = BytesIO()
         image.save(buffer, format="png")
@@ -142,25 +127,24 @@ class Contributor:
         """
         Posts contributor result image to Twitter.
         """
-        auth = tweepy.OAuthHandler(
-            global_.TWITTER["CONSUMER_KEY"], global_.TWITTER["CONSUMER_SECRET"]
+        t = Twitter(
+            auth=OAuth(
+                global_.TWITTER["ACCESS_TOKEN"],
+                global_.TWITTER["ACCESS_TOKEN_SECRET"],
+                global_.TWITTER["CONSUMER_KEY"],
+                global_.TWITTER["CONSUMER_SECRET"],
+            )
         )
-        auth.set_access_token(
-            global_.TWITTER["ACCESS_TOKEN"], global_.TWITTER["ACCESS_TOKEN_SECRET"]
-        )
-        api = tweepy.API(auth)
-        api.update_status_with_media(
-            status=f"{self.login} has scored {self.pr_count}!",
-            file=self.image_bytes,
-            filename="contributor.png",
-        )
-        
-        t = Twitter(auth=OAuth(global_.TWITTER["ACCESS_TOKEN"], global_.TWITTER["ACCESS_TOKEN_SECRET"], global_.TWITTER["CONSUMER_KEY"], global_.TWITTER["CONSUMER_SECRET"]))
-        with open("contributor.png", "rb") as imagefile:
-            imagedata = imagefile.read()
-        
-        t_upload = Twitter(domain='upload.twitter.com', auth=OAuth(global_.TWITTER["ACCESS_TOKEN"], global_.TWITTER["ACCESS_TOKEN_SECRET"], global_.TWITTER["CONSUMER_KEY"], global_.TWITTER["CONSUMER_SECRET"]))
-        id_img1 = t_upload.media.upload(media=imagedata)["media_id_string"]
-        
-        t.statuses.update(status="Hello World!", media_ids=",".join([id_img1]))
 
+        t_upload = Twitter(
+            domain="upload.twitter.com",
+            auth=OAuth(
+                global_.TWITTER["ACCESS_TOKEN"],
+                global_.TWITTER["ACCESS_TOKEN_SECRET"],
+                global_.TWITTER["CONSUMER_KEY"],
+                global_.TWITTER["CONSUMER_SECRET"],
+            ),
+        )
+        id_img1 = t_upload.media.upload(media=self.image_bytes)["media_id_string"]
+
+        t.statuses.update(status="Hello World!", media_ids=",".join([id_img1]))
