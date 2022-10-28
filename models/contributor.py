445300886cea8cc5ad4986dd0ab70c0fea07bd1a
Diwash <diwashdahal75@gmail.com>
2022-10-28 21:14:28 +0545

    update discord text

--- models/contributor.py
+++ models/contributor.py
@@ -129,7 +129,7 @@ class Contributor:
         webhook = DiscordWebhook(
             url=bot_settings.discord_hook,
             content="The top contributor of this month is "
-            + f"`{self.login}` with {self.pr_count} merged prs and {self.issue_count} opened issues.\n\n@everyone",
+            + f"`{self.login}` with {self.pr_count} merged prs and {self.issue_count} opened issues.\n@everyone",
         )
         webhook.add_file(file=self.image_bytes, filename="contributor.png")
         webhook.execute()
